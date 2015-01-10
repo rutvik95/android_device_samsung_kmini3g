2ed05d8763276e80162d32b23b3529e605436a31
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2015-01-10 17:56:47 +0530

    add kernel source and configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,8 @@ TARGET_BOOTLOADER_BOARD_NAME := kmini3g
 BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
@@ -34,7 +36,8 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 #TARGET_PREBUILT_KERNEL := device/samsung/kmini3g/kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/kmini3g
 
-#TODO: add kernel config
-#TARGET_KERNEL_CONFIG :=
+#kernel config
+TARGET_KERNEL_CONFIG := msm8226-sec_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8228-sec_atlantic3geur_defconfig
 
 BOARD_HAS_NO_SELECT_BUTTON := true
